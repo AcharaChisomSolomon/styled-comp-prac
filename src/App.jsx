073f62174e8826cc832527ec4af5bc35f8c6f006
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-03-01 11:21:28 +0000

    practice extending styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+/* eslint-disable react/prop-types */
 import styled from "styled-components"
 
 const Button = styled.button`
-  background: ${props => props.$primary ? "#bf4f74" : "white"};
-  color: ${props => props.$primary ? "white" : "#bf4f74"};
-
+  display: inline-block;
+  color: #bf4f74;
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
@@ -11,14 +11,21 @@ const Button = styled.button`
   border-radius: 3px;
 `
 
-function App() {
+const ReverseButton = ({ children, ...props }) => (
+  <Button {...props}>
+    {children.split('').reverse()}
+  </Button>
+);
 
+function App() {
   return (
     <div>
-      <Button>Normal</Button>
-      <Button $primary>Primary</Button>
+      <Button as="a" href="#">Normal</Button>
+      <Button as={ReverseButton}>
+        Custom Button with Normal Button styles
+      </Button>
     </div>
-  )
+  );
 }
 
 export default App
