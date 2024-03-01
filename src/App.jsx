445e97a827b8b570d8effb686c6a4dcf8b3352e9
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-03-01 11:04:00 +0000

    practice using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+  background: ${props => props.$primary ? "#bf4f74" : "white"};
+  color: ${props => props.$primary ? "white" : "#bf4f74"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #bf4f74;
+  border-radius: 3px;
+`
+
 function App() {
 
   return (
-    <>
-      <h1>Welcome to Styled-component Practice</h1>
-    </>
+    <div>
+      <Button>Normal</Button>
+      <Button $primary>Primary</Button>
+    </div>
   )
 }
 
